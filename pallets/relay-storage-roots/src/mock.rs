a34e9ba41e4411ebb806cc71ee5a15ad99de157a
tmpolaczyk <44604217+tmpolaczyk@users.noreply.github.com>
2024-01-16 10:14:38 +0100

    Add pallet-storage-roots (#18)
    (cherry picked from commit 8406e802bb5a326f40198948a50555fbe0bcabce)

---
+++ pallets/relay-storage-roots/src/mock.rs
@@ -0,0 +1,204 @@
+// Copyright Moonsong Labs
+// This file is part of Moonkit.
+
+// Moonkit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonkit.  If not, see <http://www.gnu.org/licenses/>.
+
+//! A minimal runtime including the pallet-relay-storage-roots pallet
+use super::*;
+use crate as pallet_relay_storage_roots;
+use cumulus_pallet_parachain_system::{RelayChainState, RelaychainStateProvider};
+use frame_support::{construct_runtime, parameter_types, traits::Everything, weights::Weight};
+use nimbus_primitives::NimbusId;
+use sp_core::{H160, H256};
+use sp_runtime::{
+	traits::{BlakeTwo256, IdentityLookup},
+	BuildStorage, Perbill,
+};
+use sp_std::convert::{TryFrom, TryInto};
+
+pub type AccountId = H160;
+pub type Balance = u128;
+
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+construct_runtime!(
+	pub enum Test
+	{
+		System: frame_system::{Pallet, Call, Config<T>, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		RelayStorageRoots: pallet_relay_storage_roots::{Pallet, Storage},
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u32 = 250;
+	pub const MaximumBlockWeight: Weight = Weight::from_parts(1024, 1);
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub const SS58Prefix: u8 = 42;
+}
+impl frame_system::Config for Test {
+	type BaseCallFilter = Everything;
+	type DbWeight = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type Nonce = u64;
+	type Block = Block;
+	type RuntimeCall = RuntimeCall;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = BlockHashCount;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type BlockWeights = ();
+	type BlockLength = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+parameter_types! {
+	pub const ExistentialDeposit: u128 = 0;
+}
+impl pallet_balances::Config for Test {
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 4];
+	type MaxLocks = ();
+	type Balance = Balance;
+	type RuntimeEvent = RuntimeEvent;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type RuntimeHoldReason = ();
+	type FreezeIdentifier = ();
+	type MaxHolds = ();
+	type MaxFreezes = ();
+	type RuntimeFreezeReason = ();
+}
+
+pub struct PersistedValidationDataGetter;
+
+impl RelaychainStateProvider for PersistedValidationDataGetter {
+	fn current_relay_chain_state() -> RelayChainState {
+		frame_support::storage::unhashed::get(b"MOCK_PERSISTED_VALIDATION_DATA").unwrap()
+	}
+
+	#[cfg(feature = "runtime-benchmarks")]
+	fn set_current_relay_chain_state(state: RelayChainState) {
+		frame_support::storage::unhashed::put(b"MOCK_PERSISTED_VALIDATION_DATA", &state);
+	}
+}
+
+pub fn set_current_relay_chain_state(state: RelayChainState) {
+	frame_support::storage::unhashed::put(b"MOCK_PERSISTED_VALIDATION_DATA", &state);
+}
+
+parameter_types! {
+	pub const MaxStorageRoots: u32 = 4;
+}
+impl Config for Test {
+	type RelaychainStateProvider = PersistedValidationDataGetter;
+	type MaxStorageRoots = MaxStorageRoots;
+	type WeightInfo = pallet_relay_storage_roots::weights::SubstrateWeight<Test>;
+}
+
+/// Panics if an event is not found in the system log of events
+#[macro_export]
+macro_rules! assert_event_emitted {
+	($event:expr) => {
+		match &$event {
+			e => {
+				assert!(
+					crate::mock::events().iter().find(|x| *x == e).is_some(),
+					"Event {:?} was not found in events: \n {:?}",
+					e,
+					crate::mock::events()
+				);
+			}
+		}
+	};
+}
+
+/// Externality builder for pallet randomness mock runtime
+pub(crate) struct ExtBuilder {
+	/// Balance amounts per AccountId
+	balances: Vec<(AccountId, Balance)>,
+	/// AuthorId -> AccountId mappings
+	mappings: Vec<(NimbusId, AccountId)>,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> ExtBuilder {
+		ExtBuilder {
+			balances: Vec::new(),
+			mappings: Vec::new(),
+		}
+	}
+}
+
+impl ExtBuilder {
+	#[allow(dead_code)]
+	pub(crate) fn with_balances(mut self, balances: Vec<(AccountId, Balance)>) -> Self {
+		self.balances = balances;
+		self
+	}
+
+	#[allow(dead_code)]
+	pub(crate) fn with_mappings(mut self, mappings: Vec<(NimbusId, AccountId)>) -> Self {
+		self.mappings = mappings;
+		self
+	}
+
+	#[allow(dead_code)]
+	pub(crate) fn build(self) -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::<Test>::default()
+			.build_storage()
+			.expect("Frame system builds valid default genesis config");
+
+		pallet_balances::GenesisConfig::<Test> {
+			balances: self.balances,
+		}
+		.assimilate_storage(&mut t)
+		.expect("Pallet balances storage can be assimilated");
+
+		let mut ext = sp_io::TestExternalities::new(t);
+		ext.execute_with(|| System::set_block_number(1));
+		ext
+	}
+}
+
+pub const ALICE: H160 = H160::repeat_byte(0xAA);
+
+pub fn fill_relay_storage_roots<T: Config>() {
+	for i in 0..T::MaxStorageRoots::get() {
+		let relay_state = RelayChainState {
+			number: i,
+			state_root: H256::default(),
+		};
+		set_current_relay_chain_state(relay_state);
+		Pallet::<T>::set_relay_storage_root();
+	}
+
+	assert!(
+		u32::try_from(RelayStorageRootKeys::<T>::get().len()).unwrap() >= T::MaxStorageRoots::get()
+	);
+}
