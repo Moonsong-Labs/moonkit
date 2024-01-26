51622ccf028383bb5d29b9d228d147aa012723d1
Francisco Gamundi <francisco@moonsonglabs.com>
2024-01-26 14:12:12 +0100

    Cleanup

--- template/node/src/chain_spec.rs
+++ template/node/src/chain_spec.rs
@@ -101,59 +101,6 @@ pub fn local_testnet_config() -> ChainSpec {
 	properties.insert("tokenDecimals".into(), 12.into());
 	properties.insert("ss58Format".into(), 42.into());
 
-	// ChainSpec::from_genesis(
-	// 	// Name
-	// 	"Local Testnet",
-	// 	// ID
-	// 	"local_testnet",
-	// 	ChainType::Local,
-	// 	move || {
-	// 		testnet_genesis(
-	// 			// initial collators.
-	// 			vec![
-	// 				(
-	// 					get_account_id_from_seed::<sr25519::Public>("Alice"),
-	// 					get_collator_keys_from_seed("Alice"),
-	// 				),
-	// 				(
-	// 					get_account_id_from_seed::<sr25519::Public>("Bob"),
-	// 					get_collator_keys_from_seed("Bob"),
-	// 				),
-	// 			],
-	// 			vec![
-	// 				get_account_id_from_seed::<sr25519::Public>("Alice"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Bob"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Charlie"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Dave"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Eve"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-	// 				get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-	// 			],
-	// 			1000.into(),
-	// 		)
-	// 	},
-	// 	// Bootnodes
-	// 	vec![],
-	// 	// Telemetry
-	// 	None,
-	// 	// Protocol ID
-	// 	Some("template-local"),
-	// 	// Fork ID
-	// 	None,
-	// 	// Properties
-	// 	Some(properties),
-	// 	// Extensions
-	// 	Extensions {
-	// 		relay_chain: "rococo-local".into(), // You MUST set this to the correct network!
-	// 		para_id: 1000,
-	// 	},
-	// )
-
 	ChainSpec::builder(
 		moonkit_template_runtime::WASM_BINARY.expect("WASM binary was not built, please build it!"),
 		Extensions {
