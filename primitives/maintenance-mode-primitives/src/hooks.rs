2104c59b5392661d48ce97c1225c6a4451cc033e
Agusrodri <agusrodriguez2456@gmail.com>
2024-02-06 05:12:11 -0800

    add maintenance mode primitives

---
+++ primitives/maintenance-mode-primitives/src/hooks.rs
@@ -0,0 +1,68 @@
+// Copyright Moonsong Labs
+// This file is part of Moonkit.
+
+// Moonkit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonkit.  If not, see <http://www.gnu.org/licenses/>.
+
+use core::marker::PhantomData;
+use frame_support::{
+	traits::{OffchainWorker, OnFinalize, OnIdle, OnInitialize, OnRuntimeUpgrade},
+	weights::Weight,
+};
+
+#[cfg(feature = "try-runtime")]
+use sp_runtime::DispatchError;
+
+pub type BlockNumber = u32;
+
+/// The hooks we want to run in Maintenance Mode
+pub struct MaintenanceHooks<T>(PhantomData<T>);
+impl<T: OnInitialize<BlockNumber>> OnInitialize<BlockNumber> for MaintenanceHooks<T> {
+	fn on_initialize(n: BlockNumber) -> Weight {
+		T::on_initialize(n)
+	}
+}
+
+// We override onIdle for xcmQueue and dmpQueue pallets to not process messages inside it
+impl<T> OnIdle<BlockNumber> for MaintenanceHooks<T> {
+	fn on_idle(_n: BlockNumber, _max_weight: Weight) -> Weight {
+		Weight::zero()
+	}
+}
+
+impl<T: OnRuntimeUpgrade> OnRuntimeUpgrade for MaintenanceHooks<T> {
+	fn on_runtime_upgrade() -> Weight {
+		T::on_runtime_upgrade()
+	}
+	#[cfg(feature = "try-runtime")]
+	fn pre_upgrade() -> Result<Vec<u8>, sp_runtime::DispatchError> {
+		T::pre_upgrade()
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn post_upgrade(state: Vec<u8>) -> Result<(), sp_runtime::DispatchError> {
+		T::post_upgrade(state)
+	}
+}
+
+impl<T: OnFinalize<BlockNumber>> OnFinalize<BlockNumber> for MaintenanceHooks<T> {
+	fn on_finalize(n: BlockNumber) {
+		T::on_finalize(n)
+	}
+}
+
+impl<T: OffchainWorker<BlockNumber>> OffchainWorker<BlockNumber> for MaintenanceHooks<T> {
+	fn offchain_worker(n: BlockNumber) {
+		T::offchain_worker(n)
+	}
+}
