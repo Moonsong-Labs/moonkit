12d0d340305f517a6cdd9ff2f3aa3a911030045b
girazoki <gorka.irazoki@gmail.com>
2024-03-14 17:19:09 +0100

    fix tests

--- pallets/async-backing/src/mock.rs
+++ pallets/async-backing/src/mock.rs
@@ -81,12 +81,12 @@ impl pallet_timestamp::Config for Test {
 parameter_types! {
 	pub const AllowMultipleBlocksPerSlot: bool = true;
 	pub const SlotDuration: u64 = 12000;
-	type ExpectedBlockTime = ConstU64<1>;
 }
 
 impl async_backing::Config for Test {
 	type AllowMultipleBlocksPerSlot = AllowMultipleBlocksPerSlot;
 	type GetAndVerifySlot = RelaySlot;
+	type ExpectedBlockTime = ConstU64<1>;
 }
 
 /// Build genesis storage according to the mock runtime.
