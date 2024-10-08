f32e4035c5b00083accd53f0f6b829feb9b88b1e
Agusrodri <agusrodriguez2456@gmail.com>
2024-10-08 10:06:18 -0700

    Add support for `transfer_assets_using_type_and_then` extrinsic in pallet-xcm precompile (#54)
    * start adding transfer_assets_using_type_and_then to pallet-xcm precompile
    
    * add function variant for address type
    
    * use WeightLimit::Unlimited in all selectors
    
    * add remaining functions and overall refactor
    
    * remove unnecessary selectors and add a couple tests
    
    * add more tests
    
    * pr suggestions
    
    * add documentation for solidity selectors
    
    * fmt
    
    * fix test
    
    * fix rust tests compilation
    
    * add clarification for SCALE encoding in custom xcm

--- precompiles/pallet-xcm/src/tests.rs
+++ precompiles/pallet-xcm/src/tests.rs
@@ -16,11 +16,12 @@
 
 use core::str::FromStr;
 
-use crate::{mock::*, Location};
+use crate::{mock::*, Location, TransferTypeHelper};
+use parity_scale_codec::Encode;
 use precompile_utils::{prelude::*, testing::*};
 use sp_core::{H160, H256};
-use sp_weights::Weight;
 use xcm::latest::Junction::*;
+use xcm::prelude::*;
 
 fn precompiles() -> Precompiles<Runtime, (SingleAddressMatch, ForeignAssetMatch)> {
 	PrecompilesValue::get()
@@ -33,10 +34,26 @@ fn test_solidity_interface_has_all_function_selectors_documented_and_implemented
 
 #[test]
 fn selectors() {
-	assert!(PCall::transfer_assets_location_selectors().contains(&0x59df8416));
-	assert!(PCall::transfer_assets_to_para_20_selectors().contains(&0xb489262e));
-	assert!(PCall::transfer_assets_to_para_32_selectors().contains(&0x4461e6f5));
-	assert!(PCall::transfer_assets_to_relay_selectors().contains(&0xd7c89659));
+	assert!(PCall::transfer_assets_location_selectors().contains(&0x9ea8ada7));
+	assert!(PCall::transfer_assets_to_para_20_selectors().contains(&0xa0aeb5fe));
+	assert!(PCall::transfer_assets_to_para_32_selectors().contains(&0xf23032c3));
+	assert!(PCall::transfer_assets_to_relay_selectors().contains(&0x6521cc2c));
+	assert!(
+		PCall::transfer_assets_using_type_and_then_location_no_remote_reserve_selectors()
+			.contains(&0x8425d893)
+	);
+	assert!(
+		PCall::transfer_assets_using_type_and_then_location_remote_reserve_selectors()
+			.contains(&0xfc19376c)
+	);
+	assert!(
+		PCall::transfer_assets_using_type_and_then_address_no_remote_reserve_selectors()
+			.contains(&0x998093ee)
+	);
+	assert!(
+		PCall::transfer_assets_using_type_and_then_address_remote_reserve_selectors()
+			.contains(&0xaaecfc62)
+	);
 }
 
 #[test]
@@ -101,9 +118,6 @@ fn test_transfer_assets_works() {
 						]
 						.into(),
 						fee_asset_item: 0u32,
-						// As we are indicating u64::MAX in ref_time, an Unlimited variant
-						// will be applied at the end.
-						weight: Weight::from_parts(u64::MAX, 80000),
 					},
 				)
 				.expect_cost(100001001)
@@ -147,9 +161,6 @@ fn test_transfer_assets_success_when_paying_fees_with_foreign_asset() {
 						// We also act as a reserve for the foreign asset thus when can pay local
 						// fees with it.
 						fee_asset_item: 1u32,
-						// As we are indicating u64::MAX in ref_time, an Unlimited variant
-						// will be applied at the end.
-						weight: Weight::from_parts(u64::MAX, 80000),
 					},
 				)
 				.expect_cost(100001001)
@@ -192,9 +203,6 @@ fn test_transfer_assets_fails_fees_unknown_reserve() {
 						.into(),
 						// No reserve will be found for this asset.
 						fee_asset_item: 1u32,
-						// As we are indicating u64::MAX in ref_time, an Unlimited variant
-						// will be applied at the end.
-						weight: Weight::from_parts(u64::MAX, 80000),
 					},
 				)
 				.expect_no_logs()
@@ -220,7 +228,6 @@ fn test_transfer_assets_to_para_20_native_asset() {
 						beneficiary: Address(Bob.into()),
 						assets: vec![(Address(pallet_balances_address), 500.into())].into(),
 						fee_asset_item: 0u32,
-						weight: Weight::from_parts(u64::MAX, 80000),
 					},
 				)
 				.expect_cost(100001002)
@@ -247,7 +254,6 @@ fn test_transfer_assets_to_para_32_native_asset() {
 						beneficiary: H256([1u8; 32]),
 						assets: vec![(Address(pallet_balances_address), 500.into())].into(),
 						fee_asset_item: 0u32,
-						weight: Weight::from_parts(u64::MAX, 80000),
 					},
 				)
 				.expect_cost(100001002)
@@ -273,7 +279,6 @@ fn test_transfer_assets_to_relay_native_asset() {
 						beneficiary: H256([1u8; 32]),
 						assets: vec![(Address(pallet_balances_address), 500.into())].into(),
 						fee_asset_item: 0u32,
-						weight: Weight::from_parts(u64::MAX, 80000),
 					},
 				)
 				.expect_cost(100001002)
@@ -316,7 +321,6 @@ fn test_transfer_assets_to_para_20_foreign_asset() {
 						]
 						.into(),
 						fee_asset_item: 0u32,
-						weight: Weight::from_parts(u64::MAX, 80000),
 					},
 				)
 				.expect_cost(100001002)
@@ -359,7 +363,6 @@ fn test_transfer_assets_to_para_32_foreign_asset() {
 						]
 						.into(),
 						fee_asset_item: 0u32,
-						weight: Weight::from_parts(u64::MAX, 80000),
 					},
 				)
 				.expect_cost(100001002)
@@ -401,7 +404,171 @@ fn test_transfer_assets_to_relay_foreign_asset() {
 						]
 						.into(),
 						fee_asset_item: 0u32,
-						weight: Weight::from_parts(u64::MAX, 80000),
+					},
+				)
+				.expect_cost(100001002)
+				.expect_no_logs()
+				.execute_returns(());
+		});
+}
+
+#[test]
+fn test_transfer_assets_using_type_and_then_location_no_remote_reserve() {
+	ExtBuilder::default()
+		.with_balances(vec![(Alice.into(), 1000)])
+		.with_xcm_assets(vec![XcmAssetDetails {
+			location: Location::parent(),
+			admin: Alice.into(),
+			asset_id: 5u16,
+			is_sufficient: true,
+			balance_to_mint: 10000u128,
+			min_balance: 1u128,
+		}])
+		.build()
+		.execute_with(|| {
+			let dest = Location::new(1, [Parachain(2)]);
+			let destination_asset_location = Location::new(1, [Parachain(2), PalletInstance(3)]);
+			let origin_asset_location = Location::new(0, [PalletInstance(1)]);
+
+			let message: Vec<u8> = xcm::VersionedXcm::<()>::V4(Xcm(vec![ClearOrigin])).encode();
+
+			precompiles()
+				.prepare_test(
+					Alice,
+					Precompile1,
+					PCall::transfer_assets_using_type_and_then_location_no_remote_reserve {
+						dest,
+						assets: vec![
+							(origin_asset_location, 100u128.into()),
+							(destination_asset_location, 150u128.into()),
+						]
+						.into(),
+						assets_transfer_type: TransferTypeHelper::DestinationReserve as u8,
+						remote_fees_id_index: 0u8,
+						fees_transfer_type: TransferTypeHelper::LocalReserve as u8,
+						custom_xcm_on_dest: message.into(),
+					},
+				)
+				.expect_cost(100001001)
+				.expect_no_logs()
+				.execute_returns(());
+		});
+}
+
+#[test]
+fn test_transfer_assets_using_type_and_then_location_remote_reserve() {
+	ExtBuilder::default()
+		.with_balances(vec![(Alice.into(), 1000)])
+		.with_xcm_assets(vec![XcmAssetDetails {
+			location: Location::parent(),
+			admin: Alice.into(),
+			asset_id: 5u16,
+			is_sufficient: true,
+			balance_to_mint: 10000u128,
+			min_balance: 1u128,
+		}])
+		.build()
+		.execute_with(|| {
+			let dest = Location::new(1, [Parachain(2)]);
+			let relay_asset_location = Location::parent();
+
+			let message: Vec<u8> = xcm::VersionedXcm::<()>::V4(Xcm(vec![ClearOrigin])).encode();
+
+			precompiles()
+				.prepare_test(
+					Alice,
+					Precompile1,
+					PCall::transfer_assets_using_type_and_then_location_remote_reserve {
+						dest,
+						assets: vec![(relay_asset_location, 150u128.into())].into(),
+						remote_fees_id_index: 0u8,
+						custom_xcm_on_dest: message.into(),
+						remote_reserve: Location::parent(),
+					},
+				)
+				.expect_cost(100001001)
+				.expect_no_logs()
+				.execute_returns(());
+		});
+}
+
+#[test]
+fn test_transfer_assets_using_type_and_then_address_no_remote_reserve() {
+	ExtBuilder::default()
+		.with_balances(vec![(Alice.into(), 1000)])
+		.with_xcm_assets(vec![XcmAssetDetails {
+			location: Location::parent(),
+			admin: Alice.into(),
+			asset_id: 5u16,
+			is_sufficient: true,
+			balance_to_mint: 10000u128,
+			min_balance: 1u128,
+		}])
+		.build()
+		.execute_with(|| {
+			// Foreign (relay) asset with prefix [255; 18] and assetId of 5u16.
+			let asset_address =
+				H160::from_str("0xfFfFFFffFffFFFFffFFfFfffFfFFFFFfffFF0005").unwrap();
+
+			// We send the native currency of the origin chain and pay fees with it.
+			let pallet_balances_address = H160::from_low_u64_be(2050);
+
+			let message: Vec<u8> = xcm::VersionedXcm::<()>::V4(Xcm(vec![ClearOrigin])).encode();
+
+			precompiles()
+				.prepare_test(
+					Alice,
+					Precompile1,
+					PCall::transfer_assets_using_type_and_then_address_no_remote_reserve {
+						dest: Location::parent(),
+						assets: vec![
+							(Address(pallet_balances_address), 500.into()),
+							(Address(asset_address), 500.into()),
+						]
+						.into(),
+						assets_transfer_type: TransferTypeHelper::DestinationReserve as u8,
+						remote_fees_id_index: 0u8,
+						fees_transfer_type: TransferTypeHelper::LocalReserve as u8,
+						custom_xcm_on_dest: message.into(),
+					},
+				)
+				.expect_cost(100001002)
+				.expect_no_logs()
+				.execute_returns(());
+		});
+}
+
+#[test]
+fn test_transfer_assets_using_type_and_then_address_remote_reserve() {
+	ExtBuilder::default()
+		.with_balances(vec![(Alice.into(), 1000)])
+		.with_xcm_assets(vec![XcmAssetDetails {
+			location: Location::parent(),
+			admin: Alice.into(),
+			asset_id: 5u16,
+			is_sufficient: true,
+			balance_to_mint: 10000u128,
+			min_balance: 1u128,
+		}])
+		.build()
+		.execute_with(|| {
+			// Foreign (relay) asset with prefix [255; 18] and assetId of 5u16.
+			let asset_address =
+				H160::from_str("0xfFfFFFffFffFFFFffFFfFfffFfFFFFFfffFF0005").unwrap();
+
+			let dest = Location::new(1, [Parachain(2)]);
+			let message: Vec<u8> = xcm::VersionedXcm::<()>::V4(Xcm(vec![ClearOrigin])).encode();
+
+			precompiles()
+				.prepare_test(
+					Alice,
+					Precompile1,
+					PCall::transfer_assets_using_type_and_then_address_remote_reserve {
+						dest,
+						assets: vec![(Address(asset_address), 500.into())].into(),
+						remote_fees_id_index: 0u8,
+						custom_xcm_on_dest: message.into(),
+						remote_reserve: Location::parent(),
 					},
 				)
 				.expect_cost(100001002)
