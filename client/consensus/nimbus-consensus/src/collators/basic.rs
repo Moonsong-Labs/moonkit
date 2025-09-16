85410b77f30e363a61c722c0dbf1066eeaa4001c
Rodrigo Quelhas <22591718+RomarQ@users.noreply.github.com>
2025-09-16 14:07:16 +0100

    add authoring_duration to basic collator (#82)

--- client/consensus/nimbus-consensus/src/collators/basic.rs
+++ client/consensus/nimbus-consensus/src/collators/basic.rs
@@ -65,6 +65,8 @@ pub struct Params<Proposer, BI, ParaClient, RClient, CIDP, CS, ADP = ()> {
 	/// The collator service used for bundling proposals into collations and announcing
 	/// to the network.
 	pub collator_service: CS,
+	/// The amount of time to spend authoring each block.
+	pub authoring_duration: Duration,
 }
 
 /// Run bare Nimbus consensus as a relay-chain-driven collator.
